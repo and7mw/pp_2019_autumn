0617816cf84746b2c8a72e0801bbc080e8e3a653
and7mw <and7mw@gmail.com>
2019-12-11 00:52:07 +0300

    fix 0.1

--- modules/task_3/andronov_m_radix_with_batcher_split/radix_with_batcher_split.h
+++ modules/task_3/andronov_m_radix_with_batcher_split/radix_with_batcher_split.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Andronov Maxim
-#ifndef MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_
-#define MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_
+#ifndef MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_RADIX_WITH_BATCHER_SPLIT_H_
+#define MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_RADIX_WITH_BATCHER_SPLIT_H_
 
 #include <mpi.h>
 #include <vector>
@@ -12,4 +12,4 @@ std::vector<int> RadixSort(std::vector<int> array);
 std::vector<int> ParallelRadixSortBatcherSplit(std::vector<int> array,
                                                     int size_arr);
 
-#endif  // MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_
+#endif  // MODULES_TASK_3_ANDRONOV_M_RADIX_WITH_BATCHER_SPLIT_RADIX_WITH_BATCHER_SPLIT_H_
